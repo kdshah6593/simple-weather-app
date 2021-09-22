1a25ba779b076fd1eb9d031ebf866f93d91e1005
Kunal <kdshah6593@gmail.com>
2021-09-22 15:18:39 -0400

    updated text content based on new way data was coming thru props, organized text content, image size

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -6,38 +6,46 @@ import Typography from '@mui/material/Typography';
 
 const WeatherCard = (props) => {
     
-    const date = new Date(props.data.dt_txt)
+  //props.data[0] is date
+  //props.data[1] is temp / max
+  //props.data[2] is min temp
+  //props.data[3] is type of weather
+  //props.data[4] is description of image
+  //props.data[5] is icon
+  //props.data[6] is mean temp
 
-    const temp = Math.round(props.data.main.temp)
-    const feels_like = Math.round(props.data.main.feels_like)
-    // const minTemp = Math.round(props.data.main.min)
-    // const maxTemp = Math.round(props.data.main.max)
+    const date = props.data[0]
+    const temp = Math.round(props.data[1])
+    const minTemp = Math.round(props.data[2])
+    const meanTemp = Math.round(props.data[6])
 
     return (
         <Card sx={{ maxWidth: 300 }}>
           <CardMedia
             component="img"
-            image={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
-            alt={props.data.weather[0].description}
+            height="180"
+            image={`http://openweathermap.org/img/wn/${props.data[5]}@2x.png`}
+            alt={props.data[4]}
+            sx={{}}
           />
           <CardContent>
-            <Typography gutterBottom variant="h3" component="div">
+            <Typography variant="h5" color="text.secondary" mt={-2}>
+              {props.data[3]}
+            </Typography>
+            <Typography gutterBottom variant="h3" component="div" mb={-1}>
               {temp}°F
             </Typography>
-            <Typography variant="h5" color="text.secondary">
-              {props.data.weather[0].main}
+            <Typography variant="h6" color="text.secondary" mb={-1}>
+              Low: {minTemp}°F
+            </Typography>
+            <Typography variant="h6" color="text.secondary" >
+              Avg: {meanTemp}°F
+            </Typography>
+            <Typography variant="body1" color="text.secondary" mt={2}>
+              {date.toLocaleString('default', {weekday: 'long'})}
             </Typography>
             <Typography variant="body1" color="text.secondary">
-              Feels Like: {feels_like}°F
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {date.toLocaleDateString()} - {date.toLocaleTimeString()}
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              Min:
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              Max:
+              {date.toLocaleString('default', {month: 'long'})} {date.getDate()}, {date.getFullYear()}
             </Typography>
           </CardContent>
         </Card>
