5b508e8e1166572fc741c898852eaddc6e41ee9f
Kunal <kdshah6593@gmail.com>
2021-09-22 17:45:29 -0400

    added background image; added fetch for current days temp; add current temp component; adjusted some styling

--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -3,16 +3,27 @@ import SearchForm from '../components/SearchForm'
 import WeatherList from '../components/WeatherList'
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import WeatherImage from '../images/weather-background.png';
+import CurrentTemp from '../components/CurrentTemp';
 
 const WeatherContainer = () => {
     const [weatherData, setWeatherData] = useState("")
     const [currentCity, setCurrentCity] = useState("")
+    const [currentWeatherData, setCurrentWeatherData] = useState("")
     
+    const currentUrl = "http://api.openweathermap.org/data/2.5/weather?"
     const baseUrl = "http://api.openweathermap.org/data/2.5/forecast?"
     const urlEnding = `&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
 
     const fetchByCity = (cityName) => {
-        fetch(baseUrl+`q=${cityName}`+ urlEnding)
+        fetch(currentUrl +`q=${cityName}`+ urlEnding)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setCurrentWeatherData(data);
+        })
+        
+        fetch(baseUrl +`q=${cityName}`+ urlEnding)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -22,7 +33,14 @@ const WeatherContainer = () => {
     }
 
     const fetchByZipCode = (zipCode) => {
-        fetch(baseUrl+`zip=${zipCode},us`+ urlEnding)
+        fetch(currentUrl +`zip=${zipCode},us`+ urlEnding)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setCurrentWeatherData(data);
+        })
+        
+        fetch(baseUrl +`zip=${zipCode},us`+ urlEnding)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -32,7 +50,14 @@ const WeatherContainer = () => {
     }
 
     const fetchByCoordinates = (lon, lat) => {
-        fetch(baseUrl+`lat=${lat}&lon=${lon}`+ urlEnding)
+        fetch(currentUrl +`lat=${lat}&lon=${lon}`+ urlEnding)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setCurrentWeatherData(data);
+        })
+        
+        fetch(baseUrl +`lat=${lat}&lon=${lon}`+ urlEnding)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -49,14 +74,17 @@ const WeatherContainer = () => {
         }
     }
 
-    const cityText = <Typography variant='h4' sx={{color: '#2196f3', fontWeight: 'bold'}}>{currentCity}'s 5-Day Forecast</Typography>
+    const cityText = <Typography mb={1} variant='h4' sx={{color: '#2196f3', fontWeight: 'bold'}}>{currentCity}'s 5-Day Forecast</Typography>
     
     return (
-        <Container maxWidth="false" sx={{height: "100vh", backgroundColor: "#e0e0e0", p: 2}}>
-            <Typography variant='h2' sx={{color: '#2196f3', fontWeight: 'bold'}}>Simple Weather App</Typography>
-            <Typography variant='h4' gutterBottom="true" sx={{color: '#2196f3'}}>Look up the forecast for the next 5 days</Typography>
+        <Container maxWidth="false" sx={{height: "100vh", backgroundColor: "#e0e0e0"}}>
+            <Container fixed sx={{height: "25vh", backgroundImage: `url(${WeatherImage})`, backgroundSize: "50% auto", backgroundRepeat: "center"}}>
+                <Typography variant='h2' sx={{color: '#2196f3', fontWeight: 'bold'}}>Five Day Forecast App</Typography>
+                <Typography variant='h4' gutterBottom="true" sx={{color: '#2196f3'}}>Look up the forecast for the next 5 days</Typography>
+            </Container>
             <SearchForm fetchByCity={fetchByCity} fetchByZipCode={fetchByZipCode} fetchByCoordinates={fetchByCoordinates} />
             {currentCity === "" ? "" : cityText}
+            {currentWeatherData === "" ? "" : <CurrentTemp weatherData={currentWeatherData} />}
             <WeatherList weatherData={weatherData} />
         </Container>
     )
