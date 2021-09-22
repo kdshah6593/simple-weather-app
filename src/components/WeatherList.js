9f75596ac4c73c635e69e65983f6ac780511c0af
Kunal <kdshah6593@gmail.com>
2021-09-22 13:21:36 -0400

    make cards into grid format; testing how it looks with 5 cards

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import WeatherCard from './WeatherCard';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 const WeatherList = (props) => {
     
@@ -7,18 +9,30 @@ const WeatherList = (props) => {
         if (props.weatherData === "") {
             return(<p>No location currently</p>)
         } else {
+            const weatherPoints = [];
+            for (let i = 0; i < props.weatherData.list.length; i += 8) {
+                weatherPoints.push(props.weatherData.list[i])
+            }
+            
+            const weatherCards1 = weatherPoints.map(dataPoint => (
+                <Grid item xs={2}>
+                    <WeatherCard data={dataPoint} />
+                </Grid>
+            ))
+            
             const weatherCards = props.weatherData.list.map(dataPoint => (
                 <WeatherCard data={dataPoint} />
             ))
-            return weatherCards
+            return weatherCards1
         }
     }
     
     return (
-        <div>
-            <p>The Weather Cards are listed below</p>
-            {forecast()}
-        </div>
+        <Box sx={{flexGrow: 1, mt: 5}}>
+            <Grid container sx={{justifyContent: 'center'}}>
+                {forecast()}
+            </Grid>
+        </Box>
     )
 }
 
