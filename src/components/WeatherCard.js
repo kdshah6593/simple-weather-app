e7039f6b3a2e05bb84d42c75ddb63f9146803ad7
Kunal <kdshah6593@gmail.com>
2021-09-22 10:46:56 -0400

    set up weather card content

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-const WeatherCard = () => {
+const WeatherCard = (props) => {
+    
+
     return (
-        <div>
-            
-        </div>
-    )
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={}
+            alt={}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Data and Time: 
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Temperature:
+            </Typography>
+          </CardContent>
+        </Card>
+      );
 }
 
 export default WeatherCard
