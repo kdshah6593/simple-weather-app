f8a63ea0399628bf4a0e684fba713c480f2dd32d
Kunal <kdshah6593@gmail.com>
2021-09-22 10:22:09 -0400

    setup initial skeleton framework

---
+++ src/containers/WeatherContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WeatherContainer = () => {
+    return (
+        <div>
+            <p>This is a weather container</p>
+            <p>It will hold search form</p>
+            <p>And it will hold the weather cards</p>
+        </div>
+    )
+}
+
+export default WeatherContainer
