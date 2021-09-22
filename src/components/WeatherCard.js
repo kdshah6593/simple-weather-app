4b5fac65db7172b1bc13131f8fbae3faff4a1488
Kunal <kdshah6593@gmail.com>
2021-09-22 12:16:26 -0400

    more values to weather card, rounded temp values

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -8,20 +8,37 @@ const WeatherCard = (props) => {
     
     const date = new Date(props.data.dt * 1000)
 
+    const temp = Math.round(props.data.main.temp)
+    const feels_like = Math.round(props.data.main.feels_like)
+    // const minTemp = Math.round(props.data.main.min)
+    // const maxTemp = Math.round(props.data.main.max)
+
     return (
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             component="img"
-            height="140"
+            height="200"
             image={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`}
             alt={props.data.weather[0].description}
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
+            <Typography gutterBottom variant="h3" component="div">
+              {temp}°F
+            </Typography>
+            <Typography variant="h5" color="text.secondary">
+              {props.data.weather[0].main}
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Feels Like: {feels_like}°F
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
               Data and Time: {date.toLocaleDateString()} - {date.toLocaleTimeString()}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Temperature: {props.data.main.temp}
+            <Typography variant="body1" color="text.secondary">
+              Min:
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Max:
             </Typography>
           </CardContent>
         </Card>
