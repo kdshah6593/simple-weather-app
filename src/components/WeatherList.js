52451fbbb792f6a31c7dc3488de984b335ca9b30
Kunal <kdshah6593@gmail.com>
2021-09-22 10:48:47 -0400

    map through data points and create a card for each one

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const WeatherList = () => {
+    
+    const forecast = props.weatherData.list.map(dataPoint => (
+        <WeatherCard data={dataPoint} />
+    ))
+    
     return (
         <div>
             <p>And it will hold the weather cards</p>
+            {forecast}
         </div>
     )
 }
