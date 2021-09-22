5196979085cbc982b8352cffa90a3dc1cd7d437f
Kunal <kdshah6593@gmail.com>
2021-09-22 17:51:31 -0400

    display nothing for current temp if city not found

--- src/components/CurrentTemp.js
+++ src/components/CurrentTemp.js
@@ -4,25 +4,36 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 const CurrentTemp = (props) => {    
+    
+    const displayCurrentTemp = () => {
+        if (props.weatherData.message === "Nothing to geocode" || props.weatherData.message === "city not found") {
+            return ("")
+        } else {
+            return (
+                <Paper sx={{width: '50%', marginLeft:"25%", marginRight:"25%", display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <Typography variant="h5" color="text.secondary" ml={1}>
+                        Currently: 
+                    </Typography>
+                    <Container sx={{display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-between"}} >
+                        <img
+                            src={`http://openweathermap.org/img/wn/${props.weatherData.weather[0].icon}@2x.png`}
+                            alt={props.weatherData.weather[0].description}
+                        />
+                        <Typography variant="h5" color="text.secondary">
+                            {props.weatherData.weather[0].main}
+                        </Typography>
+                        <Typography variant="h4" color="text.secondary">
+                            {Math.round(props.weatherData.main.temp)}°F
+                        </Typography>
+                    </Container>
+                </Paper>
+            )
+        }
+    }
+
     return (
         <Container>
-            <Paper sx={{width: '50%', marginLeft:"25%", marginRight:"25%", display:"flex", flexDirection:"row", alignItems:"center"}}>
-                <Typography variant="h5" color="text.secondary" ml={1}>
-                    Currently: 
-                </Typography>
-                <Container sx={{display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-between"}} >
-                    <img
-                        src={`http://openweathermap.org/img/wn/${props.weatherData.weather[0].icon}@2x.png`}
-                        alt={props.weatherData.weather[0].description}
-                    />
-                    <Typography variant="h5" color="text.secondary">
-                        {props.weatherData.weather[0].main}
-                    </Typography>
-                    <Typography variant="h4" color="text.secondary">
-                        {Math.round(props.weatherData.main.temp)}°F
-                    </Typography>
-                </Container>
-            </Paper>
+            {displayCurrentTemp()}
         </Container>
     )
 }
