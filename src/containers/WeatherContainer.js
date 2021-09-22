dba1e86eccb61f2e45cf30de3fe30b78b1259898
Kunal <kdshah6593@gmail.com>
2021-09-22 10:43:50 -0400

    create different fetch requests that change state and pass down those functions and data to components

--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchForm from '../components/SearchForm'
 import WeatherList from '../components/WeatherList'
 
 const WeatherContainer = () => {
+    const [weatherData, setWeatherData] = useState()
+    
+    const baseUrl = "http://api.openweathermap.org/data/2.5/forecast?"
+    const urlEnding = `&appid=${process.env.REACT_APP_API_KEY}`
+
+    const fetchByCity = (cityName) => {
+        fetch(baseUrl+`q=${cityName}`+ urlEnding)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setWeatherData(data);
+        })
+    }
+
+    const fetchByZipCode = (zipCode) => {
+        fetch(baseUrl+`zip=${zipCode},us`+ urlEnding)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setWeatherData(data);
+        })
+    }
+
+    const fetchByCoordinates = (lat, lon) => {
+        fetch(baseUrl+`lat=${lat}&lon=${lon}`+ urlEnding)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setWeatherData(data);
+        })
+    }
+    
     return (
         <div>
             <p>This is a weather container</p>
-            <SearchForm />
-            <WeatherList />
+            <SearchForm fetchByCity={fetchByCity} fetchByZipCode={fetchByZipCode} fetchByCoordinates={fetchByCoordinates} />
+            <WeatherList weatherData={weatherData} />
         </div>
     )
 }
