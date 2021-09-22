77186057a3b218ee8353d342e43e02f67711969c
Kunal <kdshah6593@gmail.com>
2021-09-22 16:44:06 -0400

    add city name when searched; no name will appear if city can't be found

--- src/containers/WeatherContainer.js
+++ src/containers/WeatherContainer.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react';
 import SearchForm from '../components/SearchForm'
 import WeatherList from '../components/WeatherList'
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const WeatherContainer = () => {
     const [weatherData, setWeatherData] = useState("")
+    const [currentCity, setCurrentCity] = useState("")
     
     const baseUrl = "http://api.openweathermap.org/data/2.5/forecast?"
     const urlEnding = `&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
@@ -14,6 +16,7 @@ const WeatherContainer = () => {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            setCity(data)
             setWeatherData(data);
         })
     }
@@ -23,6 +26,7 @@ const WeatherContainer = () => {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            setCity(data)
             setWeatherData(data);
         })
     }
@@ -32,16 +36,29 @@ const WeatherContainer = () => {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            setCity(data)
             setWeatherData(data);
         })
     }
+
+    const setCity = (data) => {
+        if (data.message === "Nothing to geocode" || data.message === "city not found") {
+            setCurrentCity("")
+        } else {
+            setCurrentCity(data.city.name)
+        }
+    }
+
+    const cityText = <Typography variant='h4' sx={{color: '#2196f3', fontWeight: 'bold'}}>{currentCity}'s 5-Day Forecast</Typography>
     
     return (
-        <div>
-            <Typography variant='h1' gutterBottom="true" sx={{color: 'orange'}}>Simple Weather App</Typography>
+        <Container maxWidth="false" sx={{height: "100vh", backgroundColor: "#e0e0e0", p: 2}}>
+            <Typography variant='h2' sx={{color: '#2196f3', fontWeight: 'bold'}}>Simple Weather App</Typography>
+            <Typography variant='h4' gutterBottom="true" sx={{color: '#2196f3'}}>Look up the forecast for the next 5 days</Typography>
             <SearchForm fetchByCity={fetchByCity} fetchByZipCode={fetchByZipCode} fetchByCoordinates={fetchByCoordinates} />
+            {currentCity === "" ? "" : cityText}
             <WeatherList weatherData={weatherData} />
-        </div>
+        </Container>
     )
 }
 
